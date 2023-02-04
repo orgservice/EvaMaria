6e20fc9c40f7da29f97fff20c21e02c51c833e9a
uhdking <124318949+uhdking@users.noreply.github.com>
2023-02-04 10:24:20 +0530

    ok

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -139,7 +139,6 @@ def encode_file_id(s: bytes) -> str:
 def encode_file_ref(file_ref: bytes) -> str:
     return base64.urlsafe_b64encode(file_ref).decode().rstrip("=")
 
-
 def unpack_new_file_id(new_file_id):
     """Return file_id, file_ref"""
     decoded = FileId.decode(new_file_id)
